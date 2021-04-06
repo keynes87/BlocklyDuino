358f573ec3e0cacce27835d12bd3eea821427128
keynes87 <keynes87@gmail.com>
2021-04-06 10:27:19 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,29 +1,29 @@
 Blockly.Arduino.esp32_bluetooth = function(block) {
   var dropdown_bt_type = block.getFieldValue('BT_type');
-  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
+  var value_name = Blockly.Arduino.valueToCode(block, 'NAME', Blockly.Arduino.ORDER_ATOMIC);
+  // TODO: Assemble Arduino into code variable.
   var code = '...;\n';
   return code;
 };
 
 Blockly.Arduino.esp32_data_available'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
+  // TODO: Assemble Arduino into code variable.
   var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.Arduino.ORDER_NONE];
 };
 
 Blockly.Arduino.esp32_recieve_data = function(block) {
-  // TODO: Assemble JavaScript into code variable.
+  // TODO: Assemble Arduino into code variable.
   var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.Arduino.ORDER_NONE];
 };
 
 Blockly.Arduino.esp32_send_data = function(block) {
-  var value_data = Blockly.JavaScript.valueToCode(block, 'data', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
+  var value_data = Blockly.Arduino.valueToCode(block, 'data', Blockly.Arduino.ORDER_ATOMIC);
+  // TODO: Assemble Arduino into code variable.
   var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  return [code, Blockly.Arduino.ORDER_NONE];
 };
