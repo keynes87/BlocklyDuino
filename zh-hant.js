c7116c047250ef03350fd2b898de1c7bcf4f1a73
keynes87 <keynes87@gmail.com>
2021-04-09 16:05:17 +0800

    Add files via upload

--- zh-hant.js
+++ zh-hant.js
@@ -1,6 +1,6 @@
-Blockly.Msg.set_BT_Type = "設定藍牙模式為"
-Blockly.Msg.BT_name = "名稱"
-Blockly.Msg.data_available = "是否從藍牙接收到資料?"
-Blockly.Msg.recieve_data = "讀取藍牙設備資料(數值)"
-Blockly.Msg.send_data = "傳送資料"
-Blockly.Msg.on_bluetooth = "到藍牙裝置"
+Blockly.Msg.set_BT_Type = "設定藍牙模式為";
+Blockly.Msg.BT_name = "名稱";
+Blockly.Msg.data_available = "是否從藍牙接收到資料?";
+Blockly.Msg.recieve_data = "讀取藍牙設備資料(數值)";
+Blockly.Msg.send_data = "傳送資料";
+Blockly.Msg.on_bluetooth = "到藍牙裝置";
